b5bd646340c408b796dc94ba61244ce9c55d8be8
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-02-08 14:24:37 -0500

    Made the addqueen method.

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,6 +1,7 @@
-
+public class QueenBoard {
 //Instance Variables:
 private int[][]board;
+//private boolean threatened;
 
 //Constructor:
 //sets all the spaces in the board to 0.
@@ -16,9 +17,22 @@ public QueenBoard(int size){
 
 //Private methods (suggested):
 private boolean addQueen(int r, int c){
-  if
+  for (int i = 0; i < board.length; i++) {
+    for (int x = 0; x < board[i].length; x++) {
+      if (board[i][x] == board[i+1][x]) {
+        return true;
+      }
+      if (board[i][x] == board [i+1][x+1]){
+      return true;
+    }
+  }
 }
-private boolean removeQueen(int r, int c)
+}
+
+private boolean removeQueen(int r, int c){
+  s
+
+}
 
 
 //Public Methods:
@@ -53,10 +67,14 @@ public String toString(){
 *        true when the board is solveable, and leaves the board in a solved state
 *@throws IllegalStateException when the board starts with any non-zero value
 */
-public boolean solve(){}
+public boolean solve(){
+  if ()
+
+}
 
 /**
 *@return the number of solutions found, and leaves the board filled with only 0's
 *@throws IllegalStateException when the board starts with any non-zero value
 */
 public int countSolutions(){}
+}
